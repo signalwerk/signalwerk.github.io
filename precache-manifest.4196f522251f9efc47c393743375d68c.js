75400796f5811e19bd0825405dced24b2e45a375
Travis CI <sh@signalwerk.ch>
2019-10-23 17:04:08 +0000

    Deploy to GitHub Pages: c505e01dd906614358c855ffc0c8e5dd6ea4a850

---
+++ precache-manifest.4196f522251f9efc47c393743375d68c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e4665e88d6434beddde",
+    "url": "/static/css/main.f1a074bf.chunk.css"
+  },
+  {
+    "revision": "4e4665e88d6434beddde",
+    "url": "/static/js/main.4e4665e8.chunk.js"
+  },
+  {
+    "revision": "893b9876b2e5a0b95a41",
+    "url": "/static/css/1.42143754.chunk.css"
+  },
+  {
+    "revision": "893b9876b2e5a0b95a41",
+    "url": "/static/js/1.893b9876.chunk.js"
+  },
+  {
+    "revision": "6549658253cf86958eb3",
+    "url": "/static/js/2.65496582.chunk.js"
+  },
+  {
+    "revision": "a9fc396a353ecfdde367",
+    "url": "/static/js/3.a9fc396a.chunk.js"
+  },
+  {
+    "revision": "b3b0d9d689fbf9493aea",
+    "url": "/static/js/4.b3b0d9d6.chunk.js"
+  },
+  {
+    "revision": "480b2d0f1ce47c8ebb9d",
+    "url": "/static/js/runtime~main.480b2d0f.js"
+  },
+  {
+    "revision": "81d0487ba73afd292730e6f89e83c2ea",
+    "url": "/static/media/open-sans-v15-latin-ext_latin-regular.81d0487b.woff2"
+  },
+  {
+    "revision": "2b6f63fce9104d1223d83dd12cd6038e",
+    "url": "/static/media/open-sans-v15-latin-ext_latin-regular.2b6f63fc.woff"
+  },
+  {
+    "revision": "15df1fb3e82321d94a0ca758c62e25d2",
+    "url": "/static/media/open-sans-v15-latin-ext_latin-700.15df1fb3.woff2"
+  },
+  {
+    "revision": "efe9ead0aecdedc597ec9d4e745e0a58",
+    "url": "/static/media/open-sans-v15-latin-ext_latin-700.efe9ead0.woff"
+  },
+  {
+    "revision": "6a4f74eb3f42d0a050c4c4eac4b4f91b",
+    "url": "/static/media/StefanHuber1673k.6a4f74eb.jpg"
+  },
+  {
+    "revision": "e7250b6acb4c967a82f5608198c24730",
+    "url": "/static/media/signalwerk.e7250b6a.svg"
+  },
+  {
+    "revision": "d502e92bef95355a6167cef524feca34",
+    "url": "/index.html"
+  }
+];
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Stefan Huber · Curriculum Vitae
+[![Build Status](https://travis-ci.org/signalwerk/signalwerk.github.io.svg?branch=build)](https://travis-ci.org/signalwerk/signalwerk.github.io)
+
+Curriculum Vitae of [→ Stefan Huber](https://signalwerk.github.io). To see the source code see [build branch](https://github.com/signalwerk/signalwerk.github.io/tree/build).
+
+### Thanks
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
